27c48d64344b7a07c6ce154f69ddafbaa36644b7
Magda Barthofer <magda.barthofer@gmail.com>
2020-03-09 19:33:31 +0100

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 root'home#index'
 get 'home/house', to: 'home#house'
 get 'home/queendom', to: 'home#queendom'
+get 'home/asgoodaspossible', to: 'home#asgoodaspossible'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
